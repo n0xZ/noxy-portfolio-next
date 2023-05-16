e73c370c69c2df3b117dca7f2ef1449e0469a1f4
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-05-16 13:13:58 -0300

    setup project utils

---
+++ src/utils/projects.ts
@@ -0,0 +1,46 @@
+export type Project = {
+	title: string
+	link: string
+}
+export const getProjects = (): Project[] => [
+	{
+		title: 'Mockxny',
+		link: 'https://mockxny.vercel.app/',
+	},
+	{
+		title: 'React Quiz',
+		link: 'https://quiz-react-typescript.vercel.app/',
+	},
+	{
+		title: 'Guess Pokemon',
+		link: 'https://guess-pokemon-solid.vercel.app/',
+	},
+	{
+		title: 'Giphyx',
+		link: 'https://giphyx.vercel.app/',
+	},
+	{
+		title: 'Fyx',
+		link: 'https://fyx-dev.netlify.app/',
+	},
+	{
+		title: 'Herox',
+		link: 'https://herox-beta.vercel.app/',
+	},
+	{
+		title: 'Notyx',
+		link: 'https://notyx.vercel.app/',
+	},
+	{
+		title: 'Codyx',
+		link: 'https://noxy-codyx.vercel.app/',
+	},
+	{
+		title: 'Nexy',
+		link: 'https://noxy-nexy.netlify.app/',
+	},
+	{
+		title: 'Moden',
+		link: 'https://noxy-moden.netlify.app/',
+	},
+]
