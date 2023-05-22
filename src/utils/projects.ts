b9b62282bb800cbd343ea630ebc58782b5622430
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-05-22 20:21:32 -0300

    add stack field on project

--- src/utils/projects.ts
+++ src/utils/projects.ts
@@ -1,7 +1,9 @@
+type Stack = 'React' | 'Vue' | 'Svelte' | 'SolidJS'
 export type Project = {
 	title: string
 	link: string
 	description: string
+	stack: Stack
 }
 export const getProjects = (): Project[] => [
 	{
@@ -9,71 +11,84 @@ export const getProjects = (): Project[] => [
 		link: 'https://mockxny.vercel.app/',
 		description:
 			'Shopping cart , where the user can select its products, an perform its payment.',
+		stack: 'React',
 	},
 	{
 		title: 'React Quiz',
 		link: 'https://quiz-react-typescript.vercel.app/',
 		description:
 			'Quiz that contains differents questions from differents genres, such as Entertainment, Science, History, etc.',
+		stack: 'React',
 	},
 	{
 		title: 'Guess Pokemon',
 		link: 'https://guess-pokemon-solid.vercel.app/',
 		description:
 			'Pokemon game, where the user must visualize an image from an Pokemon, and must guess which one is it.',
+		stack: 'SolidJS',
 	},
 	{
 		title: 'Giphyx',
 		link: 'https://giphyx.vercel.app/',
 		description:
 			'Web-app where you can find differents gifs from Giphy, and save your favourite ones.',
+		stack: 'React',
 	},
 	{
 		title: 'Fyx',
 		link: 'https://fyx-dev.netlify.app/',
 		description: 'Web-app, where you can find information about Studio Ghibli.',
+		stack: 'Vue',
 	},
 	{
 		title: 'Herox',
 		link: 'https://herox-beta.vercel.app/',
 		description:
 			'Web-app, where you can find differents heroes from DC/Marvel, create an team based on heroes from those comics.',
+		stack: 'React',
 	},
 	{
 		title: 'Notyx',
 		link: 'https://notyx.vercel.app/',
 		description: 'Web-app where you can create an collection of notes.',
+		stack: 'React',
 	},
 	{
 		title: 'Noxy recommendations ',
 		link: 'https://noxy-codyx.vercel.app/',
 		description:
 			'Web-app, where you  can create your recommendations, such as movies, anime, manga, etc.',
+		stack: 'Svelte',
 	},
 	{
 		title: 'Nexy',
 		link: 'https://noxy-nexy.netlify.app/',
 		description: 'Website, where you can create your projects, by markdown.',
+		stack: 'Vue',
 	},
 	{
 		title: 'Moden',
 		link: 'https://noxy-moden.netlify.app/',
-		description: 'Website where you can get an weather based your located',
+		description: 'Website where you can get an weather based your location',
+		stack: 'SolidJS',
 	},
 
 	{
 		title: 'Noxy feriados',
 		link: 'https://noxy-feriados.vercel.app/',
 		description: 'Web-app, where you can find the holidays from your country.',
+		stack: 'React',
 	},
 	{
 		title: 'Noxy chat',
 		link: 'https://noxy-chat.netlify.app/',
 		description: 'Chat app, where you can chat with your friends.',
+		stack: 'Vue',
 	},
 	{
 		title: 'Noxy secrets',
 		link: 'https://noxy-secrets.vercel.app/',
 		description: 'Web-app, where you can save your env secrets.',
+		stack: 'React',
 	},
 ]
